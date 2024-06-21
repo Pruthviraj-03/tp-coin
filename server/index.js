78c178a412d04cf3ef4150fd1430c755ec741508
Pruthviraj Kurane <165901916+Pruthviraj-03@users.noreply.github.com>
2024-06-21 19:36:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,10 +8,6 @@ dotenv.config({
 
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!'); 
-});
-
 app.listen(process.env.PORT || 8000, () => {
   console.log(`⚙️  Server is running at port : ${process.env.PORT || 8000}`);
 });
